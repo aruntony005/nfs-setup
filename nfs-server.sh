f5820e6e9d3505ee24b8442d010f1dc41f455947
aruntony005 <64554015+aruntony005@users.noreply.github.com>
2021-05-27 23:07:11 +0530

    Update nfs-server.sh

--- nfs-server.sh
+++ nfs-server.sh
@@ -3,5 +3,10 @@ NFS_DIR=$2
 yum install nfs-utils -y
 systemctl start nfs-server
 mkdir -p $NFS_DIR
-echo "$NFS_DIR ${CLIENT_IP}(rw,sync,root_squash)" >> /etc/exports
+if [ $CLIENT_IP == 'all' ]
+then
+  echo "$NFS_DIR '*'(rw,sync,root_squash)" >> /etc/exports
+else
+  echo "$NFS_DIR ${CLIENT_IP}(rw,sync,root_squash)" >> /etc/exports
+fi
 exportfs -arv
